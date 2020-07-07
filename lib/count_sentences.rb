1c4b41851161bc80892de4036b1b06e35ed8d583
zachary mines <zmines@wesleyan.edu>
2020-07-07 01:38:11 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
-  end
+    a = self.split()
+    d = 0 
+    a.each do |items|
+      if items.end_with?(".") || items.end_with?("?") || items.end_with?("!")
+        d += 1 
+      end 
+    end 
+    d 
+  end 
 end
