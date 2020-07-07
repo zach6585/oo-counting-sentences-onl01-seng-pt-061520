e0d11b582ab0ee51602c94f300f88bbf9fc37416
zachary mines <zmines@wesleyan.edu>
2020-07-07 01:27:46 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,28 +15,7 @@ class String
   end
 
   def count_sentences
-    a = self.split(".")
-    b = self.split("!")
-    c = self.split("?")
-    d = 0 
-    a.each do |sen|
-      if sen != ""
-        d += 1 
-      end 
-    end 
-    b.each do |sen|
-      if sen != ""
-        d += 1 
-      end 
-    end 
-    
-    c.each do |sen|
-      if sen != ""
-        d += 1 
-      end 
-    end 
-    d 
-    binding.pry
+    a = self.split("."||"?"||"!")
+    a.size 
   end
-  
 end
